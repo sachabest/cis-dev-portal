55ef4303b72f350c9443dfab46f187975830edd1
Sacha Best <sachabest@outlook.com>
2016-02-04 11:44:52 -0800

    Initial

---
+++ cis599portal/cis599portal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cis599portal.MvcApplication" Language="C#" %>
---
+++ cis599portal/cis599portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cis599portal.Startup))]
+namespace cis599portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
