798a2d2fd41d1091c60a4dce7455659ed446e3ab
Sacha Best <sachabest@outlook.com>
2016-02-04 12:12:07 -0800

    jk lets do asp5

--- cis599portal/cis599portal/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cis599portal.MvcApplication" Language="C#" %>
---
+++ cis599portal5/src/cis599portal5/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cis599portal5.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
